0146b38559116ba2bf3a4cce71eace1dff298ec2
dzroberto72 <dzroberto72@gmail.com>
2025-08-25 21:43:49 -0600

    Nuevo Proyecto

---
+++ Clase Garaje/proyecto Garaje/Garaje.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using GarajeApp.Clases;
+
+namespace GarajeApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crear garaje
+            Garaje g1 = new Garaje(1, "Garaje principal", 20, 2);
+
+            Console.WriteLine($"Garaje #{g1.NumeroGaraje} - {g1.Descripcion}");
+            Console.WriteLine($"Capacidad total: {g1.CapacidadEspacios}");
+            Console.WriteLine($"Espacios ocupados: {g1.EspaciosOcupados}");
+            Console.WriteLine($"Espacios disponibles: {g1.EspaciosDisponibles()}");
+
+            Console.WriteLine("\n--- Actualizando espacios ---");
+            Console.Write("Ingrese cantidad de espacios a ocupar: ");
+            int solicitados = int.Parse(Console.ReadLine());
+
+            g1.ActualizarEspacios(solicitados);
+
+            Console.WriteLine($"\nEspacios disponibles ahora: {g1.EspaciosDisponibles()}");
+
+            Console.WriteLine("\nPresione cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Clase Garaje/proyecto Garaje/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GarajeApp.Clases
+{
+    public class Garaje
+    {
+        // Atributos
+        public int NumeroGaraje { get; set; }
+        public string Descripcion { get; set; }
+        public int CapacidadEspacios { get; set; }
+        public int EspaciosOcupados { get; set; }
+
+        // Constructor
+        public Garaje(int numeroGaraje, string descripcion, int capacidadEspacios, int espaciosOcupados)
+        {
+            NumeroGaraje = numeroGaraje;
+            Descripcion = descripcion;
+            CapacidadEspacios = capacidadEspacios;
+            EspaciosOcupados = espaciosOcupados;
+        }
+
+        // Método para obtener espacios disponibles
+        public int EspaciosDisponibles()
+        {
+            return CapacidadEspacios - EspaciosOcupados;
+        }
+
+        // Método para actualizar espacios ocupados
+        public void ActualizarEspacios(int solicitados)
+        {
+            if (EspaciosOcupados + solicitados <= CapacidadEspacios)
+            {
+                EspaciosOcupados += solicitados;
+                Console.WriteLine($"Se han ocupado {solicitados} espacios. Ahora hay {EspaciosOcupados} ocupados.");
+            }
+            else
+            {
+                Console.WriteLine("No hay suficientes espacios disponibles en el garaje.");
+            }
+        }
+    }
+}
